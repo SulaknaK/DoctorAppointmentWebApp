1de5b0ede6c34dab26a878b96bcd9e31bfbf1ee1
Sulakna Karunaratna <sulakna.karunaratna@arimaclanka.com>
2025-02-23 23:41:49 -0500

    Appointments Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -43,6 +43,22 @@ function Navbar() {
                   </div>
                 :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
             }
+            <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt=''  />
+            
+            {/*----------------mobile--------------------*/}
+            
+            <div className='md:hidden right-0 top-0 boottom-0 z-20 overflow-hidden bg-white transition-all'> 
+                <div className='flec items-center justify-between px-5 py-6'>
+                    <img className='w-36' src={assets.logo}></img>
+                    <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon}></img>
+                </div>
+                <ul className='flex flex-col items-center gap-2 mt-5 px-5 tet-lg'>
+                    <NavLink onClick={()=>setShowMenu(false)} to='/'><p className={'px-4 py-2 rounded inline-block'} >Home</p></NavLink>
+                    <NavLink onClick={()=>setShowMenu(false)} to='/doctors'><p className={'px-4 py-2 rounded inline-block'} >All Doctors</p></NavLink>
+                    <NavLink onClick={()=>setShowMenu(false)} to='/about'><p className={'px-4 py-2 rounded inline-block'} >About</p></NavLink>
+                    <NavLink onClick={()=>setShowMenu(false)} to='/contact'><p className={'px-4 py-2 rounded inline-block'} >Contact</p> </NavLink>
+                </ul>
+            </div>
         </div>
     </div>
   )
